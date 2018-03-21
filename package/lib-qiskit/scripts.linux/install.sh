abb73fb24a11efcf0f51ea9c3bfb5769f8bd7c66
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-03-21 19:41:50 +0000

    Make it compile on MacOSX
    1) Pass the selected compiler into make (NB: have to use CC instead of CXX!)
    2) Switched from -t to --prefix (to support Pythons installed via brew) and soft-linking into the tree
    3) General clean-up of sections that are no longer needed

--- package/lib-qiskit/scripts.linux/install.sh
+++ package/lib-qiskit/scripts.linux/install.sh
@@ -16,13 +16,21 @@
 echo "**************************************************************"
 echo "Installing Quantum Software Development Kit ..."
 
-# Check extra stuff
+    # This is where pip2/pip3 will install the modules.
+    # It has its own funny structure we don't control :
+    #
+PY_DEPS_TREE=${INSTALL_DIR}/py_deps
+
+    # This is the link that *will* be pointing at the directory with modules.
+    # However, because we want to use asterisk expansion, we will create
+    # the link itself *after* PY_DEPS_TREE has been already populated.
+    #
 export PACKAGE_LIB_DIR=${INSTALL_DIR}/build
 
 ######################################################################################
 echo ""
-echo "Removing '${PACKAGE_LIB_DIR}' ..."
-rm -rf ${PACKAGE_LIB_DIR}
+echo "Removing '${PY_DEPS_TREE}' ..."
+rm -rf ${PY_DEPS_TREE} ${PACKAGE_LIB_DIR}
 
 ######################################################################################
 # Print info about possible issues
@@ -30,22 +38,11 @@ echo ""
 echo "Note that you sometimes need to upgrade your pip to the latest version"
 echo "to avoid well-known issues with user/system space installation:"
 
-SUDO="sudo "
-if [[ ${CK_PYTHON_PIP_BIN_FULL} == /home/* ]] ; then
-  SUDO=""
-fi
-
-# Check if has --system option
-${CK_PYTHON_PIP_BIN_FULL} install --help > tmp-pip-help.tmp
-if grep -q "\-\-system" tmp-pip-help.tmp ; then
- SYS=" --system"
-fi
-rm -f tmp-pip-help.tmp
-
 ######################################################################################
 echo "Compiling QISKit Simulator ..."
+
 cd ${INSTALL_DIR}/src/src/qiskit-simulator/
-make # CC=${CK_CC_PATH_FOR_CMAKE} CXX=${CK_CXX_PATH_FOR_CMAKE}
+make CC=${CK_CXX} # CC=${CK_CXX_PATH_FOR_CMAKE}
 
 if [ "${?}" != "0" ] ; then
   echo "Error: installation failed!"
@@ -56,11 +53,16 @@ fi
 echo "Installing QISKit to '${PACKAGE_LIB_DIR}' ..."
 
 cd ${INSTALL_DIR}/src
-#sudo -H env CC=${CK_CXX} ${CK_PYTHON_BIN} -m pip install -r requirements.txt --prefix=${PROJECTQ_LIB_DIR}  -U --no-cache-dir
-${CK_PYTHON_PIP_BIN_FULL} install --upgrade -r requirements.txt qiskit -t ${PACKAGE_LIB_DIR}
-#${CK_PYTHON_PIP_BIN_FULL} install qiskit -t ${PACKAGE_LIB_DIR} 
+
+#${CK_PYTHON_PIP_BIN_FULL} install --upgrade -r requirements.txt qiskit --prefix=${PY_DEPS_TREE}
+${CK_PYTHON_BIN} -m pip install -r requirements.txt qiskit --prefix=${PY_DEPS_TREE} --no-cache-dir --ignore-installed
 
 if [ "${?}" != "0" ] ; then
   echo "Error: installation failed!"
   exit 1
 fi
+
+    # In order for the asterisk to expand properly,
+    # we have to do it AFTER the directory tree has been populated:
+    #
+ln -s $PY_DEPS_TREE/lib/python*/site-packages ${PACKAGE_LIB_DIR}
