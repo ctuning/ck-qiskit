82e6eb8e122177e1a3d577505e87f8c2a1452f1c
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-09-21 16:55:34 +0100

    renamed dvdt_test_qasm.py -> run_qasm_file.py

--- program/qiskit-demo/run_qasm_file.py
+++ program/qiskit-demo/run_qasm_file.py
@@ -6,10 +6,10 @@ This example parses a given QASM file and runs it remotely, the local simulator 
 ## Running this script using the "lightweight" CK infrastructure to import Qiskit library...
 
 # 1) on remote simulator (need the API Token from IBM QuantumExperience) :
-    CK_IBM_BACKEND=ibmq_qasm_simulator ck virtual `ck search env:* --tags=qiskit,lib`  `ck search env:* --tags=ibmqx,login` --shell_cmd=dvdt_test_qasm.py
+    CK_IBM_BACKEND=ibmq_qasm_simulator ck virtual `ck search env:* --tags=qiskit,lib`  `ck search env:* --tags=ibmqx,login` --shell_cmd=run_qasm_file.py
 
 # 2) on remote quantum hardware (need the API Token from IBM QuantumExperience) :
-    CK_IBM_BACKEND=ibmqx4 ck virtual `ck search env:* --tags=qiskit,lib`  `ck search env:* --tags=ibmqx,login` --shell_cmd=dvdt_test_qasm.py
+    CK_IBM_BACKEND=ibmqx4 ck virtual `ck search env:* --tags=qiskit,lib`  `ck search env:* --tags=ibmqx,login` --shell_cmd=run_qasm_file.py
 """
 
 from IBMQuantumExperience import IBMQuantumExperience
